a2985fccd7c4402b885da880560ba857ceb9efbc
Bethwel Kiplimo <bethwelkiplimo@Bethwels-MacBook-Pro.local>
2020-11-03 03:25:56 -0500

    add start

--- start.sh
+++ start.sh
@@ -6,3 +6,4 @@ export MAIL_PASSWORD=2
 # python3.7 manage.py db upgrade 
 
 python3.7 manage.py server
+# env LDFLAGS="-I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib" pip install psycopg2
