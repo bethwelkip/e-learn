f7d1f18ef8584ddbe4ecb8003c88497a8fdc2f21
sawe-daisy <deesawe16@gmail.com>
2020-11-04 13:02:00 +0300

    quiz form

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,3 +3,11 @@ from wtforms import RadioField, TextAreaField, PasswordField, SubmitField, Boole
 from wtforms.validators import Required, Email, EqualTo
 from wtforms import ValidationError
 
+class quizForm(FlaskForm):
+    Subject=RadioField('Label', choices=[('English', 'English'),('Science', 'Science'),('Kiswahili','Kiswahili'),('Math','Math')],validators=[Required()])
+    A= TextAreaField('A', validators=[Required()])
+    B= TextAreaField('B', validators=[Required()])
+    C= TextAreaField('C', validators=[Required()])
+    D= TextAreaField('D', validators=[Required()])
+    Answer= TextAreaField('Answer', validators=[Required()])
+    Submit =SubmitField('Submit')
