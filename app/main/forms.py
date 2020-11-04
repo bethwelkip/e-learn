29bf0f5bc8c386cac041243aba771b7bd9b597c2
sawe-daisy <deesawe16@gmail.com>
2020-11-04 12:11:37 +0300

    forms

---
+++ app/main/forms.py
@@ -0,0 +1,5 @@
+from flask_wtf import FlaskForm
+from wtforms import RadioField, TextAreaField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import Required, Email, EqualTo
+from wtforms import ValidationError
+
