0fdf22cefa8e60d76b7f3a684dac67499225b3ca
bethwelkip <48167977+bethwelkip@users.noreply.github.com>
2020-11-05 04:18:10 -0500

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-
-python3.7 manage.py db migrate -m "add column \"seen\" "
-python3.7 manage.py db upgrade 
-
-# python3.7 manage.py server
-#env LDFLAGS="-I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib" pip install psycopg2
