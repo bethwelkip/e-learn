7bdaa9cd3332106b7dfd9976afb104db8cbbbe1e
sawe-daisy <deesawe16@gmail.com>
2020-11-05 11:51:04 +0300

    manage.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://bethwelkiplimo:password@localhost/<db>'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://bethwelkiplimo:password@localhost/elearn'
     SQLALCHEMY_TRACK_MODIFICATIONS  = False
 
     #  email configurations
