221e6f673fa1cc544ae8df348b199dfdaf7f2fe9
sawe-daisy <deesawe16@gmail.com>
2020-11-05 12:20:17 +0300

    quiz form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import RadioField, TextAreaField, PasswordField, SubmitField, BooleanField
+from wtforms import RadioField, TextAreaField,StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import Required, Email, EqualTo
 from wtforms import ValidationError
 
 class quizForm(FlaskForm):
+    Grade = StringField('Grade', validators=[Required()])
     Subject=RadioField('Label', choices=[('English', 'English'),('Science', 'Science'),('Kiswahili','Kiswahili'),('Math','Math')],validators=[Required()])
     Question=TextAreaField('Question', validators=[Required()])
     A= TextAreaField('A', validators=[Required()])
